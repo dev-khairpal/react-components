15acd2de81edbb8f0cb32d716882c6329cc743c8
dev-khairpal <133212101+dev-khairpal@users.noreply.github.com>
2025-01-12 15:15:39 +0530

    Added Button component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Button from "./components/Button";
+import { FcBiohazard } from "react-icons/fc";
+function App() {
+  return (
+    <div>
+      <Button primary><FcBiohazard />Primary</Button>
+      <Button secondary>Secondary</Button>
+
+      <Button danger rounded>
+        Danger
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) => {
+
+  const buttonClass = clsx(
+    "text-white border m-2 px-4 py-2 flex",
+    {
+      "border-blue-700 bg-blue-500": primary,
+      "border-gray-700 bg-gray-500": secondary,
+      "border-green-700 bg-green-500": success,
+      "border-yellow-700 bg-yellow-500": warning,
+      "border-red-700 bg-red-500": danger,
+    },
+    {
+      "border-2": outline,
+      "rounded-full": rounded,
+    }
+  )
+
+  return <button className={buttonClass}>{children}</button>
+};
+
+
+export default Button;
